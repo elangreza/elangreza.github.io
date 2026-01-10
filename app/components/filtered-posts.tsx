c4c831515ef69a84b7000be9d7e307b5de3b7ff1
mrezaelange <mrezaelange@outlook.com>
2026-01-10 12:07:59 +0700

    feat: add defer-in-go post

--- app/components/filtered-posts.tsx
+++ app/components/filtered-posts.tsx
@@ -92,7 +92,7 @@ export function FilteredPosts({
                         href={`/blog/${post.slug}`}
                     >
                         <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                            <p className="text-neutral-600 dark:text-neutral-400 w-[200px] tabular-nums">
+                            <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
                                 {formatDate(post.metadata.publishedAt, false)}
                             </p>
                             <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
